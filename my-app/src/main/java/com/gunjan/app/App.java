2529c238fbffe169743d94da7e2b75f650124bdf
gunjan5 <patelgunjan5@gmail.com>
2015-07-02 15:25:33 -0700

    kickass logging
    Signed-off-by: gunjan5 <patelgunjan5@gmail.com>

--- my-app/src/main/java/com/gunjan/app/App.java
+++ my-app/src/main/java/com/gunjan/app/App.java
@@ -1,13 +1,25 @@
 package com.gunjan.app;
 
-/**
- * Hello world!
- *
- */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class App 
 {
+	 private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello loggers!" );
+        
+        logger.trace("Hello Humans!");
+    	
+    	String name = "Gunjan";
+    	logger.debug("Ay, {}", name);
+    	logger.info("EHhhhhh.");
+    	logger.warn("GO AWAY.");
+    	logger.error("I don't like you.");
+    	
     }
 }
