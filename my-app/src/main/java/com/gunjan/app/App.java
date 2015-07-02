480b679807e862acf277d9e956e87a6a09c2ccdd
gunjan5 <patelgunjan5@gmail.com>
2015-07-02 13:38:27 -0700

    init

---
+++ my-app/src/main/java/com/gunjan/app/App.java
@@ -0,0 +1,13 @@
+package com.gunjan.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
